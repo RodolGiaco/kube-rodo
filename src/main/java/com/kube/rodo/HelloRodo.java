378925e4ef74ddbc04b026ed0ab5b71399af17eb
rodolfo.giacomodonatto <rodol.giacomodonatto@gmail.com>
2024-04-12 19:41:29 -0300

    test

--- src/main/java/com/kube/rodo/HelloRodo.java
+++ src/main/java/com/kube/rodo/HelloRodo.java
@@ -2,6 +2,6 @@ package com.kube.rodo;
 
 public class HelloRodo {
     public static void main(String[] args) {
-        System.out.println("Hello rodo!");
+        System.out.println("Hello rodo !");
     }
 }

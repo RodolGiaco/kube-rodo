440da119afa9cf3dcc8a5d5164ec2ec5c3f28554
rodolfo.giacomodonatto <rodol.giacomodonatto@gmail.com>
2024-04-12 21:18:37 -0300

    funcionando nodeport

--- src/main/java/com/kube/rodo/HelloRodo.java
+++ src/main/java/com/kube/rodo/HelloRodo.java
@@ -1,7 +1,29 @@
 package com.kube.rodo;
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloRodo {
-    public static void main(String[] args) {
-        System.out.println("Hello rodo !");
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.start();
+
+    }
+
+    // Clase para manejar las peticiones HTTP
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello World!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }

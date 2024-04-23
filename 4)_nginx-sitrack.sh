2614a93cb86b932218af9cfe5073b6e31bf2558a
rodolfo.giacomodonatto <rodol.giacomodonatto@gmail.com>
2024-04-23 09:18:57 -0300

    ingress -> cluster-stk

---
+++ 4)_nginx-sitrack.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+kind delete cluster
+
+# create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5000'
+running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+if [ "${running}" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
+
+    #Crete Kind cluster with ingress and local docker registry
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:${reg_port}"]
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+EOF
+
+# connect the registry to the cluster network
+# (the network may already be connected)
+docker network connect "kind" "${reg_name}" || true
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+
+
+
+#Ingress controller
+kubectl apply -f ingress-controller/nginx/deploy.yaml
+#Wait until is ready
+while [[ $(kubectl get pods -l app.kubernetes.io/component=controller -n ingress-nginx --context kind-kind -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do printf "Waiting for ingress-nginx-controller pod be ready \e[91m%d s\e[0m\r" $time && sleep 1 &&((time++)); done
+sleep 60s
+
+printf "\n\e[92mingress-nginx-controller pod ready!!!\e[0m\n"
+
+#Aplica todos los manifiestos correspondientes
+kubectl apply -f deploy/
+kubectl apply -f services/
+kubectl apply -f ingress/ingress-path.yaml
+

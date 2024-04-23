bdc478ed765abb9186e779be1d95a230398ef1a3
rodolfo.giacomodonatto <rodol.giacomodonatto@gmail.com>
2024-04-23 09:34:03 -0300

    arreglando multiples deploy nginx

--- 1)_nginx-ingress.sh
+++ 1)_nginx-ingress.sh
@@ -11,8 +11,10 @@ helm install nginx-ingress ingress-nginx/ingress-nginx
 #Aplica todos los manifiestos correspondientes
 kubectl apply -f deploy/
 kubectl apply -f services/
-kubectl apply -f ingress-controller/nginx/
-sleep 100
+kubectl apply -f ingress-controller/nginx/svc-np.yaml
+#Wait until is ready
+while [[ $(kubectl get pods -l app.kubernetes.io/component=controller -n ingress-nginx --context kind-kind -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do printf "Waiting for ingress-nginx-controller pod be ready \e[91m%d s\e[0m\r" $time && sleep 1 &&((time++)); done
+sleep 60s
 kubectl apply -f ingress/ingress.yaml
 kubectl annotate ingress ingress kubernetes.io/ingress.class=nginx
 

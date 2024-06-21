56e9f7e389edc3e03e8cf22c72af706358f3f079
rodolfo.giacomodonatto <rodolfo.giacomodonatto@sitrack.com>
2024-06-21 12:49:43 -0300

    todos los cambios

--- 4)_traefik-gke.sh
+++ 4)_traefik-gke.sh
@@ -1,68 +1,28 @@
 #!/bin/bash
-kind delete cluster
-
-# create registry container unless it already exists
-reg_name='kind-registry'
-reg_port='5000'
-running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
-if [ "${running}" != 'true' ]; then
-  docker run \
-    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
-    registry:2
-fi
-
-    #Crete Kind cluster with ingress and local docker registry
-cat <<EOF | kind create cluster --config=-
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-containerdConfigPatches:
-- |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-    endpoint = ["http://${reg_name}:${reg_port}"]
-nodes:
-- role: control-plane
-  kubeadmConfigPatches:
-  - |
-    kind: InitConfiguration
-    nodeRegistration:
-      kubeletExtraArgs:
-        node-labels: "ingress-ready=true"
-  extraPortMappings:
-  - containerPort: 80
-    hostPort: 80
-    protocol: TCP
-EOF
-
-# connect the registry to the cluster network
-# (the network may already be connected)
-docker network connect "kind" "${reg_name}" || true
-
-# Document the local registry
-# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: local-registry-hosting
-  namespace: kube-public
-data:
-  localRegistryHosting.v1: |
-    host: "localhost:${reg_port}"
-    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
-EOF
-
-
 
 #Ingress controller
-kubectl apply -f ingress-controller/nginx/deploy.yaml
-#Wait until is ready
-while [[ $(kubectl get pods -l app.kubernetes.io/component=controller -n ingress-nginx --context kind-kind -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do printf "Waiting for ingress-nginx-controller pod be ready \e[91m%d s\e[0m\r" $time && sleep 1 &&((time++)); done
-sleep 60s
+printf "\n\e[92mingress-traefik-controller pod ready!!!\e[0m\n"
 
-printf "\n\e[92mingress-nginx-controller pod ready!!!\e[0m\n"
+#crear namespace beta
+kubectl create namespace beta
 
-#Aplica todos los manifiestos correspondientes
-kubectl apply -f deploy/
-kubectl apply -f services/
-kubectl apply -f ingress/ingress-path.yaml
+# Install CRD for Traefik:
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.11/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+
+# Install RBAC for Traefik:
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.11/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+
+#Install traefik
+#helm repo add traefik https://helm.traefik.io/traefik
+#helm repo update
+#helm install traefik traefik/traefik --values traefik/values.yaml --namespace beta
+kubectl apply -f ingress-controller/traefik/deploy-gke.yaml --namespace beta
+kubectl apply -f ingress-controller/traefik/svc-lb.yaml --namespace beta
+
+#Ingressroot
+#kubectl apply -f ingress-controller/traefik/middleware.yaml --namespace beta
+kubectl apply -f ingress/ingressroute-tcp.yaml --namespace beta
+kubectl apply -f ingress-controller/traefik/tlsoption.yaml
+
+#kubectl apply -f traefik/ingressroute-http.yaml --namespace beta
 
